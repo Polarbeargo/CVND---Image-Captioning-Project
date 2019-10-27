8bbbdc3b2634ce577eac34d78e551993eaebe1c6
alice <bow1226@gmail.com>
2019-10-27 16:56:13 +0800

    feat: Implement DecoderRNN
    Implement methods init, forward and sample

--- model.py
+++ model.py
@@ -23,11 +23,31 @@ class EncoderCNN(nn.Module):
 
 class DecoderRNN(nn.Module):
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
-        pass
+        super(DecoderRNN, self).__init__()
+        self.hidden_size = hidden_size
+        self.embed = nn.Embedding(vocab_size, embed_size)
+        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers=num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size, vocab_size)
     
     def forward(self, features, captions):
-        pass
+        captions_trimmed = captions[..., :-1]
+        batch_size = features.shape[0]
+        embed = self.embed(captions_trimmed)
+        inputs = torch.cat([features.unsqueeze(1), embed], 1)
+        lstm_out, self.hidden = self.lstm(inputs)
+        outputs = self.fc(lstm_out)
+        return outputs
 
     def sample(self, inputs, states=None, max_len=20):
         " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
-        pass
+        tokens = []
+        for i in range(max_len):
+            lstm_output, states = self.lstm(inputs, states)
+            out = self.fc(lstm_output.squeeze(1))
+            argmax = out.max(1)
+            ind = argmax[1].item()
+            tokens.append(ind)
+            inputs = self.embed(argmax[1].long()).unsqueeze(1)
+            if ind == 1:
+                break
+        return tokens
